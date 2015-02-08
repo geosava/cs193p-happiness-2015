8b0f406669605f2f32bfa95bb0e29742f420309e
Alessandro Marzoli <alessandromarzoli@me.com>
2015-02-08 20:57:03 +0100

    lecture 5

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Alessandro Marzoli on 08/02/15.
+//  Copyright (c) 2015 Alessandro Marzoli. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
